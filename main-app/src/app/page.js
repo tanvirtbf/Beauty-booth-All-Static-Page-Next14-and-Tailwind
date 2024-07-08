1cc5c37805440a0388a435b1fde232d58a3794d6
tanvirtbf <tanver018765@gmail.com>
2024-07-08 14:38:46 +0600

    home page change

--- main-app/src/app/page.js
+++ main-app/src/app/page.js
@@ -6,8 +6,8 @@ export default function Home() {
       <h1 className="text-6xl text-center text-orange-400">Hello World!</h1>
       <Link href={"/aboutus"} className="homeBtn">About Us</Link>
       <Link href={"/privacypolicy"} className="homeBtn">Privacy Policy Page</Link>
-      <Link href={"/terms"} className="homeBtn">Terms & Conditions Page</Link>
-      <Link href={"/returnpolicy"} className="homeBtn">Return & Refund Policy Page</Link>
+      <Link href={"/terms"} className="homeBtn">Terms Page</Link>
+      <Link href={"/returnpolicy"} className="homeBtn">Return Page</Link>
     </div>
   );
 }
