48d860f942688916193ff4d0c66f7ab0ea5bff47
tanvirtbf <tanver018765@gmail.com>
2024-07-08 17:58:27 +0600

    huge change detected

--- main-app/src/components/OwnDropDown.jsx
+++ main-app/src/components/OwnDropDown.jsx
@@ -1,20 +1,20 @@
-"use client"
-import Image from 'next/image'
-import Link from 'next/link'
-import React, { useState } from 'react'
-import Arrow from '../../public/Arrow.png'
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import Arrow from "../../public/Arrow.png";
 
 const OwnDropDown = () => {
-  const [isClick,setIsClick] = useState();
+  const [isClick, setIsClick] = useState();
   return (
-    <div className='px-[16px] sm:px-[60px] max-w-[1320px] mx-auto relative'>
-      <div className='absolute top-2 right-5'>
-        <Image src={Arrow} alt='arrow'/>
+    <div className="px-[16px] sm:px-[60px] max-w-[1320px] mx-auto relative">
+      <div className="absolute top-2 right-5">
+        <Image src={Arrow} alt="arrow" />
       </div>
       <div>
         <Link></Link>
       </div>
-      <ul className=''>
+      <ul className="">
         <li>
           <Link href={"/aboutus"}>About Us</Link>
         </li>
@@ -38,7 +38,7 @@ const OwnDropDown = () => {
         </li>
       </ul>
     </div>
-  )
-}
+  );
+};
 
-export default OwnDropDown
+export default OwnDropDown;
