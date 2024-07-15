0ce9e49b896a6a026306b61a6338816ee554de1c
tanvirtbf <tanver018765@gmail.com>
2024-07-15 14:07:20 +0600

    update

--- main-app/src/components/FinalDropDown.jsx
+++ main-app/src/components/FinalDropDown.jsx
@@ -12,24 +12,15 @@ import ReturnIcon from "../../public/returnIcon.png";
 import TermsIcon from "../../public/termsIcon.png";
 
 const FinalDropDown = () => {
+  const [divId,setDivId] = useState(0)
   const [isClick, setIsClick] = useState(true);
   const [selected, setSelected] = useState("Select");
 
   const options = [
     { id: 1, text: "About Us", href: "aboutus", icon: AboutusIcon },
-    {
-      id: 2,
-      text: "Privacy Policies",
-      href: "privacypolicy",
-      icon: PrivacyIcon,
-    },
+    { id: 2, text: "Privacy Policies",href: "privacypolicy",icon: PrivacyIcon,},
     { id: 3, text: "Terms & Conditions", href: "terms", icon: TermsIcon },
-    {
-      id: 4,
-      text: "Return & Refund Policy",
-      href: "returnpolicy",
-      icon: ReturnIcon,
-    },
+    { id: 4, text: "Return & Refund Policy",href: "returnpolicy",icon: ReturnIcon,},
     { id: 5, text: "Contact Us", href: "/", icon: ContactIcon },
     { id: 6, text: "Career", href: "/", icon: CareerIcon },
     { id: 7, text: "FAQ", href: "/", icon: FaqIcon },
@@ -38,26 +29,31 @@ const FinalDropDown = () => {
   const handleClick = (e) => {
     setIsClick(!isClick);
   };
+  const handleLinkClick = (id,text) => {
+    setSelected(text)
+    setDivId(id)
+  }
   return (
-    <div className="w-full sm:w-56 h-fit pt-1 px-0 pb-2 shadow-xx rounded-lg border order-1 sm:order-2">
-      <div className="w-full px-2 flex flex-col gap-2 relative">
+    <div className="w-full sm:w-56 h-fit pt-1 px-0 pb-2 shadow-xx rounded-lg order-1 sm:order-2">
+      <div className="w-full flex flex-col gap-2 relative">
         <div
           className={`absolute top-2 right-2 ${
             isClick ? "rotate-0" : "rotate-180"
           } cursor-pointer`}
           onClick={handleClick}
         >
-          <Image src={Arrow} />
+          <Image src={Arrow} alt={Arrow} />
         </div>
-        <div className="text-sm w-full h-9 flex items-center">{selected}</div>
+        <div className="text-sm px-2 w-full h-9 flex items-center">{`${isClick ? 'Select' : selected}`}</div>
         {isClick &&
           options.map(({ id, text, href, icon }) => (
             <Link
               key={id}
               href={href}
-              className="text-sm w-full h-9 flex items-center dropgap"
+              className={`text-sm px-2 w-full h-9 flex items-center dropgap ${id===divId ? 'dropBg' : ''}`}
+              onClick={()=> handleLinkClick(id,text)}
             >
-              <Image src={icon} />
+              <Image src={icon} alt={icon}/>
               <span>{text}</span>
             </Link>
           ))}
