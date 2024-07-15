043c4a413bf7641b42c372d0e8316683f6e35a2e
tanvirtbf <tanver018765@gmail.com>
2024-07-15 14:33:46 +0600

    bug fixing on...

--- main-app/src/components/FinalDropDown.jsx
+++ main-app/src/components/FinalDropDown.jsx
@@ -12,15 +12,25 @@ import ReturnIcon from "../../public/returnIcon.png";
 import TermsIcon from "../../public/termsIcon.png";
 
 const FinalDropDown = () => {
-  const [divId,setDivId] = useState(0)
+  const [divId, setDivId] = useState(0);
   const [isClick, setIsClick] = useState(true);
   const [selected, setSelected] = useState("Select");
 
   const options = [
     { id: 1, text: "About Us", href: "aboutus", icon: AboutusIcon },
-    { id: 2, text: "Privacy Policies",href: "privacypolicy",icon: PrivacyIcon,},
+    {
+      id: 2,
+      text: "Privacy Policies",
+      href: "privacypolicy",
+      icon: PrivacyIcon,
+    },
     { id: 3, text: "Terms & Conditions", href: "terms", icon: TermsIcon },
-    { id: 4, text: "Return & Refund Policy",href: "returnpolicy",icon: ReturnIcon,},
+    {
+      id: 4,
+      text: "Return & Refund Policy",
+      href: "returnpolicy",
+      icon: ReturnIcon,
+    },
     { id: 5, text: "Contact Us", href: "/", icon: ContactIcon },
     { id: 6, text: "Career", href: "/", icon: CareerIcon },
     { id: 7, text: "FAQ", href: "/", icon: FaqIcon },
@@ -29,10 +39,10 @@ const FinalDropDown = () => {
   const handleClick = (e) => {
     setIsClick(!isClick);
   };
-  const handleLinkClick = (id,text) => {
-    setSelected(text)
-    setDivId(id)
-  }
+  const handleLinkClick = (id, text) => {
+    setSelected(text);
+    setDivId(id);
+  };
   return (
     <div className="w-full sm:w-56 h-fit pt-1 px-0 pb-2 shadow-xx rounded-lg order-1 sm:order-2">
       <div className="w-full flex flex-col gap-2 relative">
@@ -44,16 +54,18 @@ const FinalDropDown = () => {
         >
           <Image src={Arrow} alt={Arrow} />
         </div>
-        <div className="text-sm px-2 w-full h-9 flex items-center">{`${isClick ? 'Select' : selected}`}</div>
+        <div className="text-sm px-2 w-full h-9 flex items-center">{`${
+          isClick ? "Select" : selected
+        }`}</div>
         {isClick &&
           options.map(({ id, text, href, icon }) => (
             <Link
+              onClick={() => handleLinkClick(id, text)}
               key={id}
               href={href}
-              className={`text-sm px-2 w-full h-9 flex items-center dropgap ${id===divId ? 'dropBg' : ''}`}
-              onClick={()=> handleLinkClick(id,text)}
+              className={`text-sm px-2 w-full h-9 flex items-center dropgap ${id === divId ? "dropBg" : ""}`}
             >
-              <Image src={icon} alt={icon}/>
+              <Image src={icon} alt={icon} />
               <span>{text}</span>
             </Link>
           ))}
