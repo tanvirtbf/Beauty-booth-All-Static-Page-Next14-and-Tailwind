39ab8cf14792221f4149847553a91591ac729f92
tanvirtbf <tanver018765@gmail.com>
2024-07-15 12:05:35 +0600

    update

--- main-app/src/components/FinalDropDown.jsx
+++ main-app/src/components/FinalDropDown.jsx
@@ -1,7 +1,7 @@
-"use client"
-import Image from 'next/image'
-import React, { useState } from 'react'
-import Arrow from '../../public/Arrow.png'
+"use client";
+import Image from "next/image";
+import { useState } from "react";
+import Arrow from "../../public/Arrow.png";
 import AboutusIcon from "../../public/aboutusIcon.png";
 import CareerIcon from "../../public/careerIcon.png";
 import ContactIcon from "../../public/contactusIcon.png";
@@ -11,50 +11,55 @@ import ReturnIcon from "../../public/returnIcon.png";
 import TermsIcon from "../../public/termsIcon.png";
 
 const FinalDropDown = () => {
-  const [isClick,setIsClick] = useState(false)
+  const [isClick, setIsClick] = useState(true);
   const handleClick = (e) => {
-    setIsClick(!isClick)
-  }
+    setIsClick(!isClick);
+  };
   return (
-    <div className='w-full sm:w-80 pt-1 px-0 pb-2 shadow-xx rounded-lg border'>
-      <div className='w-full px-2 flex flex-col gap-2 relative'>
-        <div className={`absolute top-2 right-2 ${isClick ? 'rotate-0' : 'rotate-180'} cursor-pointer`}>
-          <Image src={Arrow}/>
+    <div className="w-full sm:w-80 h-fit pt-1 px-0 pb-2 shadow-xx rounded-lg border">
+      <div className="w-full px-2 flex flex-col gap-2 relative">
+        <div
+          className={`absolute top-2 right-2 ${
+            isClick ? "rotate-0" : "rotate-180"
+          } cursor-pointer`}
+          onClick={handleClick}
+        >
+          <Image src={Arrow} />
         </div>
-        <div className='text-sm w-full h-9 flex items-center cursor-pointer' onClick={handleClick}>
-          Select
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={AboutusIcon}/>
-          <span>About Us</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={PrivacyIcon}/>
-          <span>Privacy Policy</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={TermsIcon}/>
-          <span>Terms & Conditions</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={ReturnIcon}/>
-          <span>Return & Refund Policy</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={ContactIcon}/>
-          <span>Contact Us</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={CareerIcon}/>
-          <span>Career</span>
-        </div>
-        <div className='text-sm w-full h-9 flex items-center dropgap'>
-          <Image src={FaqIcon}/>
-          <span>FAQ</span>
+        <div className="text-sm w-full h-9 flex items-center">Select</div>
+        <div className={`${isClick?'block':'hidden'}`}>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={AboutusIcon} />
+            <span>About Us</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={PrivacyIcon} />
+            <span>Privacy Policy</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={TermsIcon} />
+            <span>Terms & Conditions</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={ReturnIcon} />
+            <span>Return & Refund Policy</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={ContactIcon} />
+            <span>Contact Us</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={CareerIcon} />
+            <span>Career</span>
+          </div>
+          <div className="text-sm w-full h-9 flex items-center dropgap">
+            <Image src={FaqIcon} />
+            <span>FAQ</span>
+          </div>
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default FinalDropDown
+export default FinalDropDown;
