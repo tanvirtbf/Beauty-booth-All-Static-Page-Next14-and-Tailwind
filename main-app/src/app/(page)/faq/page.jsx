b9f2d8a6a209e0f33e74757cda3425e36be435c0
tanvirtbf <tanver018765@gmail.com>
2024-08-03 11:54:27 +0600

    data map complete

--- main-app/src/app/(page)/faq/page.jsx
+++ main-app/src/app/(page)/faq/page.jsx
@@ -1,8 +1,38 @@
+"use client";
 import FinalDropDown from "@/components/FinalDropDown";
 import Image from "next/image";
+import { useState } from "react";
 import FaqWebBanner from "../../../../public/faqWebBanner.png";
+import UserCircle from "../../../../public/user-cirlce-add 1.png";
 
 const Faq = () => {
+  const [isClick, setIsClick] = useState(false);
+  const data = [
+    {
+      id: 1,
+      text: "New Visitor",
+    },
+    {
+      id: 2,
+      text: "Account & Payment",
+    },
+    {
+      id: 3,
+      text: "Delivery & Return",
+    },
+    {
+      id: 4,
+      text: "Guest Checkout",
+    },
+    {
+      id: 5,
+      text: "Shopping",
+    },
+    {
+      id: 6,
+      text: "Promotions & Freebies",
+    },
+  ];
   return (
     <div className="container">
       <div className="w-full flex flex-wrap justify-between">
@@ -12,15 +42,23 @@ const Faq = () => {
         </div>
         <FinalDropDown />
         <div className="w-full sm:w-3/4 order-3">
-          <div className="grid gap-4 grid-cols-12">
-            <div>
-              
-            </div>
+          <div className="grid gap-3 sm:gap-6 grid-cols-12">
+            {data.map(({ id, text }) => (
+              <div
+                key={id}
+                className="px-5 py-6 rounded-lg border border-[#ECECEE] sm:col-span-4 col-span-6 flex gap-4 justify-start items-center"
+              >
+                <Image src={UserCircle} alt={text} priority />
+                <p className="text-xs sm:text-sm font-normal text-[#222222]">
+                  {text}
+                </p>
+              </div>
+            ))}
           </div>
         </div>
       </div>
     </div>
   );
 };
-
+// border: 1px solid #000000
 export default Faq;
