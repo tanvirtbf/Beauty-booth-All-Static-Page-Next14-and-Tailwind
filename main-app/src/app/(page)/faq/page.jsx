56e72f6913ba547ec1d2d19adf5be73a32d8cf17
tanvirtbf <tanver018765@gmail.com>
2024-08-03 10:10:59 +0600

    banner and heading complete

--- main-app/src/app/(page)/faq/page.jsx
+++ main-app/src/app/(page)/faq/page.jsx
@@ -4,8 +4,11 @@ import FaqWebBanner from "../../../../public/faqWebBanner.png";
 const Faq = () => {
   return (
     <div className="container">
-      <div className="w-full">
-        <Image src={FaqWebBanner} alt="faq Banner" priority />
+      <div className="w-full flex flex-wrap justify-between">
+        <div className="w-full flex flex-col gap-6 order-2 sm:order-1 mt-7 mb-6">
+          <h1 className="staticHeading">Frequently Asked Questions</h1>
+          <Image src={FaqWebBanner} alt="faq Banner" priority />
+        </div>
       </div>
     </div>
   );
