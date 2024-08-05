098c03d5ae208e3822a6d40cd144cde59431f0f9
tanvirtbf <tanver018765@gmail.com>
2024-08-05 11:40:23 +0600

    mobile banner width related problem solve

--- main-app/src/app/(page)/contactus/page.jsx
+++ main-app/src/app/(page)/contactus/page.jsx
@@ -9,8 +9,8 @@ const Contactus = () => {
       <div className="w-full flex flex-wrap justify-between">
         <div className="w-full flex flex-col gap-6 order-2 sm:order-1 mt-7 mb-6">
           <h1 className="staticHeading">Frequently Asked Questions</h1>
-          <Image src={WebBanner} alt="Web Banner" priority className="hidden sm:block" />
-          <Image src={MobileBanner} alt="Mobile Banner" priority className="block sm:hidden"/>
+          <Image src={WebBanner} alt="Web Banner" priority className="w-full hidden sm:block" />
+          <Image src={MobileBanner} alt="Mobile Banner" priority className="w-full block sm:hidden"/>
         </div>
         <FinalDropDown />
       </div>
