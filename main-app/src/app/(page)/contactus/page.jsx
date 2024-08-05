bcd854b4af4d7540bd1920d6c1b7a71b075b5c29
tanvirtbf <tanver018765@gmail.com>
2024-08-05 11:39:13 +0600

    Banner placed

--- main-app/src/app/(page)/contactus/page.jsx
+++ main-app/src/app/(page)/contactus/page.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
-
+"use client"
+import WebBanner from '../../../../public/contactusWebBanner.png'
+import MobileBanner from '../../../../public/contactusMobileBanner.png'
+import Image from 'next/image';
+import FinalDropDown from '@/components/FinalDropDown';
 const Contactus = () => {
   return (
-    <div>
-      <h1 className='text-center'>This is Contact us Page</h1>
+    <div className="container">
+      <div className="w-full flex flex-wrap justify-between">
+        <div className="w-full flex flex-col gap-6 order-2 sm:order-1 mt-7 mb-6">
+          <h1 className="staticHeading">Frequently Asked Questions</h1>
+          <Image src={WebBanner} alt="Web Banner" priority className="hidden sm:block" />
+          <Image src={MobileBanner} alt="Mobile Banner" priority className="block sm:hidden"/>
+        </div>
+        <FinalDropDown />
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Contactus;
