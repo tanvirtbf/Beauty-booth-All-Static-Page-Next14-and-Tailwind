8c845f1ad89f9b323ee59a4c54da49becb4b8929
tanvirtbf <tanver018765@gmail.com>
2024-08-07 17:20:23 +0600

    start navigation

--- main-app/src/app/(page)/career/page.jsx
+++ main-app/src/app/(page)/career/page.jsx
@@ -1,9 +1,11 @@
+import FinalDropDown from '@/components/FinalDropDown';
 import React from 'react'
 
 const Career = () => {
   return (
     <div>
       <h1 className='text-center'>This is Career Page</h1>
+      <FinalDropDown />
     </div>
   )
 }
